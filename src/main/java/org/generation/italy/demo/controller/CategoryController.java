8a052afcdbca84fe29a3785589fb4898b3d51fe5
Francesco <francescopanico94@libero.it>
2022-12-20 22:14:01 +0100

    add category create

--- src/main/java/org/generation/italy/demo/controller/CategoryController.java
+++ src/main/java/org/generation/italy/demo/controller/CategoryController.java
@@ -1,5 +1,32 @@
 package org.generation.italy.demo.controller;
 
-public class CategoryController {
+import java.util.List;
 
+import org.generation.italy.demo.pojo.Category;
+import org.generation.italy.demo.pojo.Foto;
+import org.generation.italy.demo.service.FotoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/category")
+public class CategoryController {
+	
+	@Autowired
+	private FotoService fotoService;
+    
+	@GetMapping("/admin/create")
+	public String create(Model model) {
+		
+		Category category = new Category();
+		model.addAttribute("category", category);
+		
+		List<Foto> photo = fotoService.findAll();
+		model.addAttribute("photo", photo);
+		
+		return "IngredientCreate";
+	}
 }
